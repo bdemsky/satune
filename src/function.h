ef44cea1f59355739c77bc687d75854294118034
Hamed <hamed.gorjiara@gmail.com>
2017-06-16 10:44:21 -0700

    adding fucntion's fields

--- src/function.h
+++ src/function.h
@@ -2,9 +2,13 @@
 #define FUNCTION_H
 #include "classlist.h"
 #include "mymemory.h"
-
+#include "ops.h"
+#include "structs.h"
 struct Function {
-    
+    enum ArithOp op;
+    VectorSet* domains; 
+    Set * range;
+    enum OverFlowBehavior overflowbehavior;
     Table* table;
 };
 #endif
