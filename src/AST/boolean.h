93d68c5fed12b2c33c73f6763c10f49bf6bf150c
bdemsky <bdemsky@uci.edu>
2017-06-21 15:12:04 -0700

    edits

--- src/AST/boolean.h
+++ src/AST/boolean.h
@@ -47,15 +47,6 @@ struct BooleanPredicate {
 Boolean * allocBoolean(VarType t);
 Boolean * allocBooleanOrder(Order * order, uint64_t first, uint64_t second);
 Boolean * allocBooleanPredicate(Predicate * predicate, Element ** inputs, uint numInputs);
-Boolean * allocBooleanLogic(LogicOp op, Boolean * left, Boolean* right);
-/**
- * This function also save new boooleans to solver->allbooleans
- * @param solver
- * @param op
- * @param array
- * @param asize
- * @return
- */
 Boolean * allocBooleanLogicArray(CSolver *solver, LogicOp op, Boolean ** array, uint asize);
 void deleteBoolean(Boolean * This);
 
