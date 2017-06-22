1c6e44bab2dcb386930511ce613e2c7b2f73f26b
bdemsky <bdemsky@uci.edu>
2017-06-22 12:19:20 -0700

    Remove extra files

--- src/Test/.buildconstraints.d
+++
@@ -1,4 +0,0 @@
-buildconstraints: buildconstraints.c ../csolver.h ../classlist.h \
-  ../mymemory.h ../config.h ../AST/ops.h ../Collections/structs.h \
-  ../Collections/vector.h ../Collections/hashtable.h ../common.h \
-  ../Collections/hashset.h ../Collections/array.h
