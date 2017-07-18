bc9c5252b1b8f8750680a80cef414143d3fd594d
Hamed <hamed.gorjiara@gmail.com>
2017-07-17 18:29:04 -0700

    Modify order test case ...

--- src/Test/testorder.c
+++ src/Test/testorder.c
@@ -6,15 +6,15 @@ int main(int numargs, char ** argv) {
 	uint64_t set1[]={5, 1, 4};
 	Set * s=createSet(solver, 0, set1, 3);
 	Order* order = createOrder(solver, TOTAL, s);
-	Boolean* b1=  orderConstraint(solver, order, 1, 4);
-	Boolean* b2=  orderConstraint(solver, order, 5, 4);
+	Boolean* b1=  orderConstraint(solver, order, 5, 1);
+	Boolean* b2=  orderConstraint(solver, order, 1, 4);
 	addConstraint(solver, b1);
 	addConstraint(solver, b2);
 	if (startEncoding(solver)==1)
-		printf("O(1,4)=%d O(5,4)=%d O(4,5)=%d O(1,5)=%d\n", 
-			getOrderConstraintValue(solver, order, 1, 4), 
+		printf("O(5,1)=%d O(1,4)=%d O(5,4)=%d O(1,5)=%d\n", 
+			getOrderConstraintValue(solver, order, 5, 1), 
+			getOrderConstraintValue(solver, order, 1, 4),
 			getOrderConstraintValue(solver, order, 5, 4),
-			getOrderConstraintValue(solver, order, 4, 5),
 			getOrderConstraintValue(solver, order, 1, 5));
 	else
 		printf("UNSAT\n");
