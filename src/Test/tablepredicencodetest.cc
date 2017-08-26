0ce5a25b09b4b54cd763758388075b1f61ccefcb
bdemsky <bdemsky@uci.edu>
2017-08-25 19:32:56 -0700

    Update test files

--- src/Test/tablepredicencodetest.cc
+++ src/Test/tablepredicencodetest.cc
@@ -59,7 +59,7 @@ int main(int numargs, char **argv) {
 	solver->addConstraint(pred2);
 
 	if (solver->startEncoding() == 1)
-		printf("e1=%llu e2=%llu e3=%llu undefFlag:%d\n",
+		printf("e1=%" PRIu64 " e2=%" PRIu64 " e3=%" PRIu64 " undefFlag:%d\n",
 					 solver->getElementValue(e1), solver->getElementValue(e2),
 					 solver->getElementValue(e3), solver->getBooleanValue(undef));
 	else
