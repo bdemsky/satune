fcd1280d92704662fec5ee8bac68cd44619cd5c2
bdemsky <bdemsky@uci.edu>
2017-10-23 19:37:06 -0700

    Autotuner test case

---
+++ src/Test/deserializerautotune.cc
@@ -0,0 +1,24 @@
+#include "csolver.h"
+#include "autotuner.h"
+
+int main(int argc, char ** argv){
+	if(argc < 2){
+		printf("You should specify file names ...");
+		exit(-1);	
+	}
+	CSolver * solvers[argc-1];
+	AutoTuner *autotuner = new AutoTuner(300);
+	for(int i = 1; i < argc; i++) {
+		solvers[i-1] = CSolver::deserialize(argv[i]);
+		autotuner->addProblem(solvers[i-1]);
+	}
+
+	autotuner->tune();
+	delete autotuner;
+	
+	for(int i = 1; i < argc; i++) {
+		delete solvers[i-1];
+	}
+
+	return 1;
+}
