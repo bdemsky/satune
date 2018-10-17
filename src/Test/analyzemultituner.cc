ef51ba7e8b758221ea07ff36a6f05449270336c4
bdemsky <bdemsky@uci.edu>
2018-10-17 14:13:47 -0700

    Add file to run parser

---
+++ src/Test/analyzemultituner.cc
@@ -0,0 +1,17 @@
+#include "csolver.h"
+#include "multituner.h"
+#include "searchtuner.h"
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		printf("You should specify number of runs\n");
+		exit(-1);
+	}
+	uint numruns;
+	sscanf(argv[1], "%u", &numruns);
+
+	MultiTuner *multituner = new MultiTuner(0, 0, 0);
+	multituner->readData(numruns);
+	delete multituner;
+	return 0;
+}
